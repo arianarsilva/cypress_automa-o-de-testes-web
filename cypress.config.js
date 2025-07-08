ccdae5f104048f38433e8c0c4a444c83f6871280
Ariana Silva <arianarebeca.silva@gmail.com>
2025-07-08 15:33:16 -0300

    alinhando com as versões da alura

--- cypress.config.js
+++ cypress.config.js
@@ -4,14 +4,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  env: {
-    "email": "clinica@gmail.com",
-    "senha": "4321",
-    "api_login": "http://localhost:8080/auth/login",
-    "api_clinica": "http://localhost:8080/clinica",
-    "api_especialista": "http://localhost:8080/especialista",
-    "requestMode": true
-  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -26,7 +18,17 @@ module.exports = defineConfig({
       json: false,
       timestamp: "mmddyyyy_HHMMss"
     },
-    
-    defaultCommandTimeout: 60000
+    projectId: "8gi3z6", //Insira a projectId pela fornecida no seu Cypress Cloud
+    defaultCommandTimeout: 60000,
+    env:{
+      "email": "clinica@gmail.com",
+      "senha": "4321",
+      "api_login": "http://localhost:8080/auth/login",
+      "api_clinica": "http://localhost:8080/clinica",
+      "api_especialista": "http://localhost:8080/especialista",
+      "requestMode": true
+    }
+
   },
+
 });
