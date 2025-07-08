8f8aeb481fedfe5ee7e6203c586770965b2d9fc5
Ariana Silva <arianarebeca.silva@gmail.com>
2025-07-08 13:41:44 -0300

    colocando cadastro de especialista no login-api

--- cypress/e2e/login-api.cy.js
+++ cypress/e2e/login-api.cy.js
@@ -15,3 +15,40 @@ describe('Testes em API', () => {
         })
     })
 })
+
+describe('Cadastro de especialista', () => {
+    beforeEach (() => {
+        cy.fixture('especialistas.json').as('especialistas')
+    })
+    
+    it('Deve realizar cadastro de um especialista via requisição POST', () => {
+        cy.get('@especialistas').then((dados) => {
+        const especialista = dados.especialistas[0];
+
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('api_clinica'),
+            body: {
+                nome: especialista.nome,
+                email: especialista.email,
+                senha: especialista.senha,
+                endereco: {
+                    cep: especialista.cep,
+                    rua: especialista.rua,
+                    numero: especialista.numero,
+                    complemento: especialista.complemento,
+                    estado: especialista.estado
+                }
+            },
+        }).then ((response) => {
+            if(response.status !== 201){
+                cy.log(`O status ${response.status} não é o padrão 201`)
+            }
+            expect(response.body).to.have.property('id')
+            expect(response.body).to.have.property('nome')
+            expect(response.body).to.have.property('email')
+        })
+        })
+    })
+
+})
